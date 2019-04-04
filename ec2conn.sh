0ab77925a8af1471c838d85d4a901042bcd3da63
kondoumh <kondoumh@gmail.com>
2019-04-04 12:45:07 +0900

    first commit

---
+++ ec2conn.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ $1 = "start" ]; then
+    echo "Starting instance"
+    startInstance
+elif [ $1 = "login" ]; then
+    echo "login"
+    login
+elif [ $1 = "stop" ]; then
+    echo "Stopping instance"
+    stopInstance
+fi
+
+
+startInstance() {
+    aws ec2 start-instances --instance-ids ${EC2_INSTANCE_ID}
+}
+
+login() {
+    ip=$(aws ec2 describe-instances --instance-ids ${EC2_INSTANCE_ID} --query "Reservations[0].Instances[0].PublicIpAddress")
+    echo ${ip}
+    ssh -i devenv-key.pem ubuntu@${ip}
+}
+
+
+stopInstance() {
+    aws ec2 stop-instances --instance-ids ${EC2_INSTANCE_ID}
+}
