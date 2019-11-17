5dcddf5907d532207ac33db079f42ebe594aec43
kondoumh <kondoumh@gmail.com>
2019-11-17 11:25:52 +0900

    Apply ssh key data

--- droplet/main.tf
+++ droplet/main.tf
@@ -2,10 +2,14 @@ provider "digitalocean" {
   # export DIGITALOCEAN_TOKEN="Your API TOKEN"
 }
 
+data "digitalocean_ssh_key" "ssh_key" {
+  name = "blink"
+}
+
 resource "digitalocean_droplet" "dev" {
   image = "${var.ubuntu}"
   name = "dev-1"
   region = "${var.do_sgp1}"
   size = "512mb"
-  ssh_keys = []
+  ssh_keys = [data.digitalocean_ssh_key.ssh_key.id]
 }
