57134473fd2dd052817ac8080ec1064eee4cfb0a
kondoumh <kondoumh@gmail.com>
2021-11-05 17:12:29 +0900

    format

--- kube-tz/demo/src/main/java/com/example/demo/DemoApplication.java
+++ kube-tz/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,24 +1,12 @@
 package com.example.demo;
 
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.time.temporal.ChronoUnit;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
-	public static void main(String[] args) {
-		var now = OffsetDateTime.now(ZoneId.systemDefault()).truncatedTo(ChronoUnit.SECONDS);
-		var now2 = OffsetDateTime.now().truncatedTo(ChronoUnit.SECONDS);
-		logger.info("now1: {}", now.toString());
-		logger.info("now2: {}", now2.toString());
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 }
