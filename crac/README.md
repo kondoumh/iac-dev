6060de3ae87140b19d1b3b400692cf51241c8db0
Masahiro Kondo <kondoumh@gmail.com>
2023-06-14 21:49:00 +0900

    add jetty example

---
+++ crac/README.md
@@ -0,0 +1,39 @@
+# CRaC Spring Boot Trial
+
+## Manualy create snapshot
+```shell
+docker build -t . crac-spring-boot-trial
+```
+
+```shell
+docker run -it --privileged --rm -p 5001:5001 --name=crac-spring-boot-trial crac-spring-boot-trial
+```
+
+```shell
+java -XX:CRaCCheckpointTo=/opt/crac-files -jar /opt/app/app.jar
+```
+
+```shell
+$ curl localhost:5001
+Hello! You are visitor number 1.
+$ curl localhost:5001
+Hello! You are visitor number 2.
+$ curl localhost:5001
+Hello! You are visitor number 3.
+$ curl localhost:5001
+Hello! You are visitor number 4.
+$ curl localhost:5001
+Hello! You are visitor number 5.
+```
+
+```shell
+docker exec -it -u root crac-spring-boot-trial /bin/bash
+```
+
+```shell
+$ jps
+74 Jps
+14 app.jar
+
+jcmd /opt/app/app.jar JDK.checkpoint
+```
---
+++ crac/example-jetty/src/main/java/com/example/App.java
@@ -0,0 +1,73 @@
+package com.example;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+import org.crac.Context;
+import org.crac.Core;
+import org.crac.Resource;
+
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+class ServerManager implements Resource
+{
+    Server server;
+    Thread preventExitThread;
+
+    public ServerManager(int port, Handler handler) throws Exception {
+        server = new Server(5001);
+        server.setHandler(handler);
+        server.start();
+        Core.getGlobalContext().register(this);
+
+        // beforeCheckpoint implemented in simplest manner: shutdown the jetty
+        // server. There may be no non-daemon threads left, so JVM may exit.
+        // Here we provides a thread that will keep JVM running.
+        preventExitThread = new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(1_000_000);
+                } catch (InterruptedException e) {
+                }
+            }
+        });
+        preventExitThread.start();
+    }
+
+    @Override
+    public void beforeCheckpoint(Context<? extends Resource> context) throws Exception {
+        server.stop();
+    }
+
+    @Override
+    public void afterRestore(Context<? extends Resource> context) throws Exception {
+        server.start();
+    }
+}
+
+public class App extends AbstractHandler
+{
+    static ServerManager serverManager;
+
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response)
+        throws IOException, ServletException
+    {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("Hello World");
+    }
+
+    public static void main( String[] args ) throws Exception
+    {
+        serverManager = new ServerManager(5001, new App());
+    }
+}
